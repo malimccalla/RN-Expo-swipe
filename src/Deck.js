ebaf8e26c76543a0db75662d4fed14ac49672fc8
Mali Michael <malimccalla@gmail.com>
2017-07-26 16:48:10 +0100

    X + deg interpoloation with spring back

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
   constructor(props) {
@@ -12,24 +14,53 @@ class Deck extends Component {
         this.position.setValue({ x: gestureState.dx , y: gestureState.dy });
       },
       onPanResponderRelease: (event, grestureState) => {
-        console.log('Released!');
+        this.resetPosition();
       }
     });
   }
 
+  resetPosition() {
+    Animated.spring(this.position, {
+      toValue: { x: 0, y: 0}
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-80deg', '0deg', '80deg']
+    })
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    }
+  }
+
   renderCards() {
-    return this.props.data.map((item) => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        )
+      }
+
       return this.props.renderCard(item);
     });
   }
 
   render() {
     return (
-      <Animated.View { ...this.panResponder.panHandlers}
-        style={this.position.getLayout()}
-      >
+      <View>
         {this.renderCards()}
-      </Animated.View>
+      </View>
     );
   }
 }
